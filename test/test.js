1bf7531807c6456c65edbb7e1ae171e714ce0f89
albert <albert.maharjan94@gmail.com>
2021-08-24 16:48:41 +0545

    test: test changes and file filtering

--- test/test.js
+++ test/test.js
@@ -219,52 +219,6 @@ describe('User Test', () => {
     })
 })
 
-
-/**
- * user : {
-        type : Schema.Types.ObjectId,
-        red : 'User',
-        required : true
-    },
-    
-    vehicle : {
-        type : String,
-         required : true
-    },
-    date : {
-        type : String,
-        required : false
-    },
-   activity : [
-       {
-    start_time : {
-        type : String,
-        required:  false,
-    },
-    end_time : {
-        type : String,
-        required : false
-    },
-    speed: {
-        type : Number,
-        required : false,
-    },
-    kilometer: {
-        type : Number,
-        required : false,
-    }
-   }],
-   avg_speed : {
-       type : Number,
-       default : 0 
-   },
-   distance : {
-       type  : Number,
-       default : 0
-   }
- */
-
-
 describe('Activity Test', () => {
     it('activityCreate', async () => {
         const activity = {
